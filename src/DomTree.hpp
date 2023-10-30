ca96e86a4bd8b80f649ecd385682cb5640843b46
Dmitrryy <da.drozdov@mail.ru>
2023-10-30 10:48:43 +0300

    [DT] Implement DomTree structure

--- src/DomTree.hpp
+++ src/DomTree.hpp
@@ -1,7 +1,40 @@
 #pragma once
 
+#include "Function.hpp"
+
 namespace son {
 
+class DomTree final {
+  std::unordered_map<RegionNodeBase *, size_t> m_nodeToIdx;
+  std::vector<RegionNodeBase *> m_idxToNode;
+  std::vector<size_t> m_idomTree;
 
+public:
+  DomTree() = default;
+  DomTree(const Function &F) { recalc(F); }
+
+  void recalc(const Function &F) {
+    auto &&dfsResult = F.dfs();
+    auto &&semi = F.semiDominators(dfsResult);
+
+    m_idomTree = F.iDominators(semi);
+    std::tie(m_idxToNode, std::ignore, m_nodeToIdx, std::ignore) = dfsResult;
+  }
+
+  bool dominates(RegionNodeBase *A, RegionNodeBase *B) {
+    const auto aIdx = m_nodeToIdx.at(A);
+    const auto bIdx = m_nodeToIdx.at(B);
+    return dominates(aIdx, bIdx);
+  }
+
+private:
+  bool dominates(size_t aIdx, size_t bIdx) {
+    // go up to root
+    while(bIdx != 0 && bIdx != aIdx) {
+      bIdx = m_idomTree[bIdx];
+    }
+    return aIdx == bIdx;
+  }
+};
 
 } // namespace son
--- tests/unit/DomTree/DomTree.cpp
+++ tests/unit/DomTree/DomTree.cpp
@@ -1,8 +1,67 @@
 #include "gtest/gtest.h"
 
-#include <Function.hpp>
+#include "graphs.hpp"
+
+#include <DomTree.hpp>
+
 using namespace son;
 
-TEST(Function, FunctionEmpty) {
+TEST(DomTree, example1) {
+  BUILD_GRAPH_TEST1(Func);
+  EXPECT_TRUE(Func.verify());
 
+  DomTree DT(Func);
+  EXPECT_TRUE(Func.verify());
+
+  const std::vector<RegionNodeBase *> Nodes = {A, B, C, D,
+                                                  E, F, G};
+  for (auto && node : Nodes) {
+    EXPECT_TRUE(DT.dominates(node, node));
+    EXPECT_TRUE(DT.dominates(A, node));
+  }
+  EXPECT_FALSE(DT.dominates(F, D));
+  EXPECT_FALSE(DT.dominates(E, D));
+  EXPECT_FALSE(DT.dominates(G, D));
+  EXPECT_FALSE(DT.dominates(C, D));
+
+  EXPECT_TRUE(DT.dominates(B, D));
+  EXPECT_TRUE(DT.dominates(B, C));
+  EXPECT_TRUE(DT.dominates(B, G));
+  EXPECT_TRUE(DT.dominates(B, E));
+  EXPECT_TRUE(DT.dominates(B, F));
+
+  EXPECT_FALSE(DT.dominates(D, B));
+  EXPECT_FALSE(DT.dominates(C, B));
+  EXPECT_FALSE(DT.dominates(G, B));
+  EXPECT_FALSE(DT.dominates(E, B));
+  EXPECT_FALSE(DT.dominates(F, B));
+}
+
+TEST(DomTree, example2) {
+  BUILD_GRAPH_TEST2(Func);
+  EXPECT_TRUE(Func.verify());
+
+  DomTree DT(Func);
+  EXPECT_TRUE(Func.verify());
+
+  const std::vector<RegionNodeBase *> Nodes = {A, B, C, D, E, F,
+                                                  G, H, I, K, J};
+  for (auto &&node : Nodes) {
+    EXPECT_TRUE(DT.dominates(node, node));
+    EXPECT_TRUE(DT.dominates(A, node));
+  }
+}
+
+TEST(DomTree, example3) {
+  BUILD_GRAPH_TEST3(Func);
+  EXPECT_TRUE(Func.verify());
+
+  DomTree DT(Func);
+  EXPECT_TRUE(Func.verify());
+
+  const std::vector<RegionNodeBase *> Nodes = {A, B, C, D, E, F, G, H, I};
+  for (auto &&node : Nodes) {
+    EXPECT_TRUE(DT.dominates(node, node));
+    EXPECT_TRUE(DT.dominates(A, node));
+  }
 }
