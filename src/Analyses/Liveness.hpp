9ef84c979e21e4691d6eb1d8e61c3126c485be0d
Dmitrryy <da.drozdov85@gmail.com>
2024-03-02 20:19:21 +0300

    Liveness Analyses

--- src/Analyses/Liveness.hpp
+++ src/Analyses/Liveness.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "Node.hpp"
 #include <Analyses/DomTree.hpp>
 #include <Analyses/Loop.hpp>
 #include <Function.hpp>
 
+#include <iterator>
 #include <unordered_map>
 
 namespace son {
@@ -14,6 +16,8 @@ public:
 
 private:
   LiveNumbers m_liveNumbers;
+  std::unordered_map<RegionNodeBase *, std::vector<Node *>> m_liveIn;
+  std::unordered_map<Node *, std::pair<size_t, size_t>> m_intervals;
 
 public:
   Liveness() = default;
@@ -29,19 +33,100 @@ public:
     // live numbers
     m_liveNumbers = _liveNumbers(linRegs, DataMap);
 
-    // intervals
-    // TODO
+    // traverse blocks in reversed linear order
+    for (auto &&RIt = linRegs.rbegin(), RItEnd = linRegs.rend(); RIt != RItEnd;
+         ++RIt) {
+      auto R = *RIt;
+      std::unordered_set<Node *> live;
+      const auto &&RSuccessors = R->successors();
+
+      // calc initial liveset for R:
+      //   union of livesets in succ blocks
+      for (auto S : RSuccessors) {
+        auto &&SLive = m_liveIn[S];
+        std::copy(SLive.begin(), SLive.end(), std::inserter(live, live.end()));
+      }
+      //   + succs phi real inputs
+      for (auto S : RSuccessors) {
+        for (auto Phi : S->phis()) {
+          // input of R
+          live.emplace(Phi->getValOf(R));
+        }
+      }
+
+      // append liverange of current BB to inst
+      const auto BBInterval = m_intervals[R];
+      for (auto ins : live) {
+        m_intervals[ins] = {
+            std::min(BBInterval.first, m_intervals[ins].first),
+            std::max(BBInterval.second, m_intervals[ins].second)};
+      }
+
+      // reverse iterate block insts (non-phi)
+      auto &&RInstrs = DataMap[R];
+      for (auto &&InstIt = RInstrs.rbegin(), ItE = RInstrs.rend();
+           InstIt != ItE; ++InstIt) {
+        // shorten the inst live interval
+        m_intervals[*InstIt].first = m_liveNumbers[*InstIt];
+        // min live interval is [ln, ln + 2)
+        if (m_intervals[*InstIt].second < m_intervals[*InstIt].first + 2) {
+          m_intervals[*InstIt].second = m_intervals[*InstIt].first + 2;
+        }
+        if (live.contains(*InstIt)) {
+          live.erase(*InstIt);
+        }
+
+        // iterate inputs
+        for (auto Input : (*InstIt)->operands()) {
+          m_intervals[Input] = {
+              std::min(BBInterval.first, m_intervals[Input].first),
+              std::max(m_liveNumbers[*InstIt], m_intervals[Input].second)};
+          live.emplace(Input);
+        }
+      }
+
+      // remove phis from current liveset
+      for (auto Phi : R->phis()) {
+        if (live.contains(Phi)) {
+          live.erase(Phi);
+        }
+        // NOTE: It is skipped in original algorithm?
+        m_intervals[Phi].first = m_liveNumbers[Phi];
+      }
+
+      // if block is loop header
+      auto *RL = LI.getLoopFor(R);
+      if (RL != nullptr && RL->header() == R) {
+        // find loop end
+        size_t loopEnd = 0;
+        for (auto B : RL->blocks()) {
+          loopEnd = std::max(loopEnd, m_intervals[B].second);
+        }
+        // add life range of [loop_header.begin, loop_end)
+        for (auto opd : live) {
+          m_intervals[opd] = {
+              std::min(BBInterval.first, m_intervals[opd].first),
+              std::max(loopEnd, m_intervals[opd].second)};
+        }
+      }
+
+      m_liveIn[R] = std::vector(live.begin(), live.end());
+    }
   }
 
   auto liveNumber(Node *node) const { return m_liveNumbers.at(node); }
+  auto liveInterval(Node *node) const { return m_intervals.at(node); }
+  auto &liveIn(RegionNodeBase *region) const & { return m_liveIn.at(region); }
 
 private:
-  static LiveNumbers _liveNumbers(const std::vector<RegionNodeBase *> &linOrder,
-                                  const Function::DataMapperTy &rangeToInstrs) {
+  LiveNumbers _liveNumbers(const std::vector<RegionNodeBase *> &linOrder,
+                           const Function::DataMapperTy &rangeToInstrs) {
     LiveNumbers res;
     size_t curLiveNum = 0;
     for (auto R : linOrder) {
+      auto liveFrom = curLiveNum;
       if (!rangeToInstrs.contains(R)) {
+        m_intervals[R] = {liveFrom, liveFrom};
         continue;
       }
 
@@ -56,6 +141,9 @@ private:
       }
 
       curLiveNum += 2;
+      auto liveTo = curLiveNum;
+
+      m_intervals[R] = {liveFrom, liveTo};
     }
 
     return res;
--- tests/unit/Liveness/Liveness.cpp
+++ tests/unit/Liveness/Liveness.cpp
@@ -69,15 +69,123 @@ TEST(Liveness, test_lecture) {
       {N1, "1"},  {N2, "2"},  {N3, "3"},         {V0, "V0"},
       {V1, "V1"}, {V2, "V2"}, {V3_N1Phi0, "V3"}, {V4_N1Phi1, "V4"},
       {V5, "V5"}, {V7, "V7"}, {V8, "V8"},        {V9, "V9"}};
-  F.nameNodes(Names);
-  F.dump(std::cout, Names);
 
-  // live numbers
+  // func i32 Liveness_test_lecture() {
+  // entry:
+  //   V0 = i32 1
+  //   V1 = i32 10
+  //   Jmp 1
+  //
+  // 1: /* Pred: entry, 2 */
+  //   V4 = i32 Phi void 1, i32 V1, i32 V8
+  //   V3 = i32 Phi void 1, i32 V0, i32 V7
+  //   V5 = i1 CmpEQ i32 V4, i32 V0
+  //   If i1 V5, T:2, F:3
+  //
+  // 2: /* Pred: 1 */
+  //   V8 = i32 Sub i32 V4, i32 V0
+  //   V7 = i32 Mul i32 V3, i32 V4
+  //   Jmp 1
+  //
+  // 3: /* Pred: 1 */
+  //   Jmp exit
+  //
+  // exit: /* Pred: 3 */
+  //   V2 = i32 20
+  //   V9 = i32 Add i32 V2, i32 V3
+  //   Ret void exit, i32 V9
+  //
+  // }
+  auto DumpNames = Names;
+  F.nameNodes(DumpNames);
+  F.dump(std::cout, DumpNames);
+
   Liveness LV(F);
-
+  // live numbers:
+  // V0 = i32 1, live: 2
+  // V1 = i32 10, live: 4
+  // V2 = i32 20, live: 18
+  // V3 = i32 Phi void 1, i32 V0, i32 V7, live: 6
+  // V4 = i32 Phi void 1, i32 V1, i32 V8, live: 6
+  // V5 = i1 CmpEQ i32 V4, i32 V0, live: 8
+  // V7 = i32 Mul i32 V3, i32 V4, live: 14
+  // V8 = i32 Sub i32 V4, i32 V0, live: 12
+  // V9 = i32 Add i32 V2, i32 V3, live: 20
+  std::cout << "live numbers:" << std::endl;
   for (auto &&n :
        std::vector<Node *>{V0, V1, V2, V3_N1Phi0, V4_N1Phi1, V5, V7, V8, V9}) {
-    n->dump(std::cout, Names);
+    n->dump(std::cout, DumpNames);
     std::cout << ", live: " << LV.liveNumber(n) << std::endl;
   }
+  EXPECT_EQ(LV.liveNumber(V0), 2);
+  EXPECT_EQ(LV.liveNumber(V1), 4);
+  EXPECT_EQ(LV.liveNumber(V2), 18);
+  EXPECT_EQ(LV.liveNumber(V3_N1Phi0), 6);
+  EXPECT_EQ(LV.liveNumber(V4_N1Phi1), 6);
+  EXPECT_EQ(LV.liveNumber(V5), 8);
+  EXPECT_EQ(LV.liveNumber(V7), 14);
+  EXPECT_EQ(LV.liveNumber(V8), 12);
+  EXPECT_EQ(LV.liveNumber(V9), 20);
+
+  // live intervals:
+  // V9, live interval = [20, 22)
+  // V8, live interval = [12, 16)
+  // V7, live interval = [14, 16)
+  // V4, live interval = [6, 14)
+  // V3, live interval = [6, 20)
+  // V1, live interval = [4, 6)
+  // V0, live interval = [2, 16)
+  // 3, live interval = [16, 16)
+  // V5, live interval = [8, 10)
+  // 2, live interval = [10, 16)
+  // V2, live interval = [18, 20)
+  // 1, live interval = [6, 10)
+  std::cout << "live intervals:" << std::endl;
+  for (auto name : Names) {
+    std::cout << name.second << ", live interval = [";
+    auto LInterval = LV.liveInterval(const_cast<Node *>(name.first));
+    std::cout << LInterval.first << ", " << LInterval.second << ")"
+              << std::endl;
+  }
+  EXPECT_EQ(LV.liveInterval(V0), std::make_pair(2LU, 16LU));
+  EXPECT_EQ(LV.liveInterval(V1), std::make_pair(4LU, 6LU));
+  EXPECT_EQ(LV.liveInterval(V2), std::make_pair(18LU, 20LU));
+  EXPECT_EQ(LV.liveInterval(V3_N1Phi0), std::make_pair(6LU, 20LU));
+  EXPECT_EQ(LV.liveInterval(V4_N1Phi1), std::make_pair(6LU, 14LU));
+  EXPECT_EQ(LV.liveInterval(V5), std::make_pair(8LU, 10LU));
+  EXPECT_EQ(LV.liveInterval(V7), std::make_pair(14LU, 16LU));
+  EXPECT_EQ(LV.liveInterval(V8), std::make_pair(12LU, 16LU));
+  EXPECT_EQ(LV.liveInterval(V9), std::make_pair(20LU, 22LU));
+
+  // live in:
+  // Inputs to region: entry
+  // Inputs to region: 1
+  //   V0 = i32 1
+  // Inputs to region: 2
+  //   V0 = i32 1
+  //   V4 = i32 Phi void 1, i32 V1, i32 V8
+  //   V3 = i32 Phi void 1, i32 V0, i32 V7
+  // Inputs to region: 3
+  //   V3 = i32 Phi void 1, i32 V0, i32 V7
+  // Inputs to region: exit
+  //   V3 = i32 Phi void 1, i32 V0, i32 V7
+  std::cout << "live in:" << std::endl;
+  for (auto R : std::vector<RegionNodeBase *>{N0, N1, N2, N3, N4}) {
+    std::cout << "Inputs to region: " << DumpNames[R] << std::endl;
+    for (auto input : LV.liveIn(R)) {
+      std::cout << "  ";
+      input->dump(std::cout, DumpNames);
+      std::cout << std::endl;
+    }
+  }
+  EXPECT_TRUE(LV.liveIn(N0).empty());
+  auto &&liveIn = LV.liveIn(N1);
+  EXPECT_EQ(std::set(liveIn.begin(), liveIn.end()), std::set<Node *>{V0});
+  auto &&liveIn_N2 = LV.liveIn(N2);
+  std::set<Node *> ref_N2{V0, V3_N1Phi0, V4_N1Phi1};
+  EXPECT_EQ(std::set(liveIn_N2.begin(), liveIn_N2.end()), ref_N2);
+  auto &&liveIn_N3 = LV.liveIn(N3);
+  EXPECT_EQ(std::set(liveIn_N3.begin(), liveIn_N3.end()), std::set<Node *>{V3_N1Phi0});
+  auto &&liveIn_N4 = LV.liveIn(N4);
+  EXPECT_EQ(std::set(liveIn_N4.begin(), liveIn_N4.end()), std::set<Node *>{V3_N1Phi0});
 }
